a769ae42e26c57c985cbbe55475c158df6db6f7d
jarradcook <jarradcook88@gmail.com>
2025-07-28 15:41:31 +1000

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const App = () => <h1>Lees Racing App</h1>
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
